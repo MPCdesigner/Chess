9fcf2ea6811c0f7c0f750af36731518353b5d284
MPCdesigner <63942604+MPCdesigner@users.noreply.github.com>
2020-04-23 01:20:54 -0700

    Add files via upload

---
+++ Piece.java
@@ -0,0 +1,41 @@
+    package chessSketch;
+
+public abstract class Piece extends Main {
+	private int posX, posY, color;
+	
+	Piece(int _posX, int _posY, int _color){
+		
+		posX = _posX;
+		posY= _posY;
+		color = _color;
+		
+	}
+	//accessors
+	
+	int getPosX() {
+		return this.posX;
+	}
+	int getPosY() {
+		return this.posY;
+	}
+	int getColor() {
+		return this.color;
+	}
+	
+	abstract char getType();
+	
+	//move
+	void makeMove(int x, int y) {
+		pList.remove(board[x][y]); 
+		capturedPieces.add(board[x][y]);
+		board[x][y] = board[this.posX][this.posY];
+		board[this.posX][this.posY]= null; 
+		this.posX = x;
+		this.posY = y;
+
+		
+	}
+	 abstract boolean checkLegal(int x1, int y1, int x2, int y2);
+}
+
+    
